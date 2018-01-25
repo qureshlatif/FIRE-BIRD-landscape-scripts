63ae4266c81a02b3e8242043f32c3a8c798feb39
Quresh Latif <qureshlatif@yahoo.com>
2018-01-25 16:30:55 -0700

    adds masking of HSI by fire perimeter
    Fire perimeters are merged for each year into a single shapefile, and then HSI mosaics are now masked by merged fire perimeter shapefiles.

--- R6/03-Mosaic_BBWO_HSIs.py
+++ R6/03-Mosaic_BBWO_HSIs.py
@@ -1,6 +1,3 @@
-# Ran into consistent memory error in 2003 at or4244112390420020713. Oddly, the BBWO model application tool ran fine with the data for this fire.
-# Switched to R version of this script to see if memory errors can be avoided.
-
 import arcpy
 from arcpy import env
 from arcpy.sa import *
@@ -12,19 +9,28 @@ arcpy.env.overwriteOutput = True
 
 #_________________Inputs__________________#
 # Base directory #
-base = "T:/FS/RD/RMRS/Science/WTE/Research/Landscape_decision_tool/R6/"
-years = range(1985, 1994) + range(1995, 2016)
+base = "T:/FS/RD/RMRS/Science/WTE/Research/Landscape_decision_tool/"
+wildfire_data_dir = "wildfire_data/"
+wildfire_years = range(1985, 1994) + range(1995, 2016)
+HSI_dir = base + "R6/"
 #_________________________________________#
 
 for t in range(0, len(years)):
-    yr_dir = base + str(years[t]) + '/'
-    fire_dirs = os.listdir(yr_dir)
+    wf_dir = base + wildfire_data_dir + str(wildfire_years[t]) + '/'
+    BB_polys = glob.glob(wf_dir + "brn_bounds/*.shp")
+    BB_polys = [bb for bb in BB_polys if bb[89:91] == 'or' or bb[89:91] == 'wa']
+    hsi_dir = HSI_dir + str(wildfire_years[t]) + '/'
     HSI_rasters = []
-    for f in range(0, len(fire_dirs)):
-        hsi = yr_dir + fire_dirs[f] + '/hsi.tif'
+    for f in range(0, len(BB_polys)):
+        fire = BB_polys[f][89:110]
+        hsi = hsi_dir + fire + '/hsi.tif'
         HSI_rasters = HSI_rasters + [hsi]
-    arcpy.MosaicToNewRaster_management(HSI_rasters, yr_dir, "HSI.tif", number_of_bands = 1)
-    del(hsi)
+    FP = arcpy.Merge_management(BB_polys, hsi_dir + "fire_perims.shp")
+    HSI0 = arcpy.MosaicToNewRaster_management(HSI_rasters, hsi_dir, "HSI_unmask.tif", number_of_bands = 1)
+    HSI = ExtractByMask(HSI0, FP)
+    HSI.save(hsi_dir + "HSI.tif")
+    del(hsi, HSI0, HSI)
+    arcpy.Delete_management(hsi_dir + "HSI_unmask.tif")
 
 
 
